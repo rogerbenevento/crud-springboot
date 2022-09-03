ddfbdfb5f0327071f4798b71e46b68313f9848aa
Rogerio Benevento Jr <rogerio@forepoint.com.br>
2022-09-03 13:02:55 -0300

    Implementada persistencia de dados no MySQL

--- src/main/java/com/example/crud/crud/Carro.java
+++ src/main/java/com/example/crud/crud/Carro.java
@@ -1,9 +1,11 @@
 package com.example.crud.crud;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -13,12 +15,20 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Table(name = "carros")
 public class Carro {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
     Long id;
+
+    @Column(name = "marca")
     String marca;
+
+    @Column(name = "modelo")
     String modelo;
+
+    @Column(name = "ano")
     Integer ano;
 }
